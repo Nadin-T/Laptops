7fe067db5becf6f76e0d6920ae3b4ed73a478b28
Nadin-T <nadezhda.tarasova8@ya.ru>
2024-07-08 01:17:52 +0300

    added file 'laptops, created class laptop'

---
+++ Laptop.java
@@ -0,0 +1,18 @@
+package Laptops;
+
+public class Laptop {
+    private String brand;
+    private String os;
+    private int ram;
+    private int sizeHHD;
+    private String color;
+
+    public Laptop(String brand, String os, int ram,
+            int sizeHHD, String color) {
+        this.brand = brand;
+        this.os = os;
+        this.ram = ram;
+        this.sizeHHD = sizeHHD;
+        this.color = color;
+    }
+}
